0dbb49c082a4a872d930eea2ea4e1a15b04f9172
Matt Wilson <matthew@synesis.com.au>
2026-08-19 22:12:36 +1000

    version

--- version.go
+++ version.go
@@ -14,7 +14,7 @@ import "github.com/synesissoftware/ver2go"
 const (
 	VersionMajor uint16 = 0
 	VersionMinor uint16 = 3
-	VersionPatch uint16 = 0
+	VersionPatch uint16 = 1
 	VersionAB    uint16 = 0xFFFF
 	Version      uint64 = (uint64(VersionMajor) << 48) + (uint64(VersionMinor) << 32) + (uint64(VersionPatch) << 16) + (uint64(VersionAB) << 0)
 )
